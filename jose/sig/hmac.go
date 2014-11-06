11c3a19653ddf30cac81bd90b8fcb7e4665f0c70
Ed Rooth <ed.rooth@coreos.com>
2014-11-06 14:42:15 -0800

    jose/sig: initial hmac support

---
+++ jose/sig/hmac.go
@@ -0,0 +1,81 @@
+package sig
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/hmac"
+	_ "crypto/sha256"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/coreos-inc/auth/jose"
+)
+
+type VerifierHMAC struct {
+	KeyID  string
+	Hash   crypto.Hash
+	Secret []byte
+}
+
+type SignerHMAC struct {
+	VerifierHMAC
+}
+
+func NewVerifierHMAC(jwk jose.JWK) (*VerifierHMAC, error) {
+	if strings.ToUpper(jwk.Alg) != "HS256" {
+		return nil, fmt.Errorf("unsupported key algorithm %q", jwk.Alg)
+	}
+
+	v := VerifierHMAC{
+		KeyID:  jwk.ID,
+		Secret: jwk.Secret,
+		Hash:   crypto.SHA256,
+	}
+
+	return &v, nil
+}
+
+func (v *VerifierHMAC) ID() string {
+	return v.KeyID
+}
+
+func (v *VerifierHMAC) Alg() string {
+	return "HS256"
+}
+
+func (v *VerifierHMAC) Verify(sig []byte, data []byte) error {
+	h := hmac.New(v.Hash.New, v.Secret)
+	h.Write(data)
+	if !bytes.Equal(sig, h.Sum(nil)) {
+		return errors.New("invalid hmac signature")
+	}
+	return nil
+}
+
+func NewSignerHMAC(kid string, secret []byte) *SignerHMAC {
+	return &SignerHMAC{
+		VerifierHMAC: VerifierHMAC{
+			KeyID:  kid,
+			Secret: secret,
+			Hash:   crypto.SHA256,
+		},
+	}
+}
+
+func (s *SignerHMAC) Sign(data []byte) ([]byte, error) {
+	h := hmac.New(s.Hash.New, s.Secret)
+	h.Write(data)
+	return h.Sum(nil), nil
+}
+
+func (s *SignerHMAC) JWK() jose.JWK {
+	return jose.JWK{
+		ID: s.ID(),
+		// TODO (sym3tri): verify the type string here
+		Type:   "HMAC",
+		Alg:    s.Alg(),
+		Use:    "sig",
+		Secret: s.Secret,
+	}
+}

d76fafeebf0244662055d251d030bc43445d2d09
Brian Waldon <bcwaldon@gmail.com>
2014-11-11 12:43:15 -0800

    server: further testing of /auth handler

--- server/http_test.go
+++ server/http_test.go
@@ -1,10 +1,28 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
+
+	"github.com/coreos-inc/auth/oauth2"
 )
+
+type fakeIDPConnector struct {
+	loginURL string
+}
+
+func (f *fakeIDPConnector) DisplayType() string {
+	return "Fake"
+}
+
+func (f *fakeIDPConnector) LoginURL(r *http.Request) string {
+	return f.loginURL
+}
+
+func (f *fakeIDPConnector) Register(mux *http.ServeMux) {}
 
 func TestHandleAuthFuncMethodNotAllowed(t *testing.T) {
 	for _, m := range []string{"POST", "PUT", "DELETE"} {
@@ -22,6 +40,69 @@ func TestHandleAuthFuncMethodNotAllowed(t *testing.T) {
 		got := w.Code
 		if want != got {
 			t.Errorf("case %s: expected HTTP %d, got %d", m, want, got)
+		}
+	}
+}
+
+func TestHandleAuthFuncResponses(t *testing.T) {
+	idpc := &fakeIDPConnector{loginURL: "http://fake.example.com"}
+	ciRepo := NewClientIdentityRepo([]oauth2.ClientIdentity{
+		oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"},
+	})
+
+	tests := []struct {
+		query        url.Values
+		wantCode     int
+		wantLocation string
+	}{
+		{
+			query: url.Values{
+				"response_type": []string{"code"},
+				"redirect_uri":  []string{"http://client.example.com/callback"},
+				"client_id":     []string{"XXX"},
+			},
+			wantCode:     http.StatusTemporaryRedirect,
+			wantLocation: "http://fake.example.com",
+		},
+
+		// nonexistant client_id
+		{
+			query: url.Values{
+				"response_type": []string{"code"},
+				"redirect_uri":  []string{"http://client.example.com/callback"},
+				"client_id":     []string{"YYY"},
+			},
+			wantCode: http.StatusBadRequest,
+		},
+
+		// ParseAuthCodeRequest should fail
+		{
+			query: url.Values{
+				"response_type": []string{"token"},
+			},
+			wantCode: http.StatusBadRequest,
+		},
+	}
+
+	for i, tt := range tests {
+		hdlr := handleAuthFunc(ciRepo, idpc)
+		w := httptest.NewRecorder()
+		u := fmt.Sprintf("http://server.example.com?%s", tt.query.Encode())
+		req, err := http.NewRequest("GET", u, nil)
+		if err != nil {
+			t.Errorf("case %d: unable to form HTTP request: %v", i, err)
+			continue
+		}
+
+		hdlr.ServeHTTP(w, req)
+		if tt.wantCode != w.Code {
+			t.Errorf("case %d: HTTP code mismatch: want=%d got=%d", i, tt.wantCode, w.Code)
+			continue
+		}
+
+		gotLocation := w.Header().Get("Location")
+		if tt.wantLocation != gotLocation {
+			t.Errorf("case %d: HTTP Location header mismatch: want=%s got=%s", i, tt.wantLocation, gotLocation)
 		}
 	}
 }

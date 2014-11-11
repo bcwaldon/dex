4276c65590c30d2164bb4eb2b533cd21aa8a6a30
Brian Waldon <bcwaldon@gmail.com>
2014-11-11 15:04:16 -0800

    server: remove unimplemented endpoints

--- oidc/provider.go
+++ oidc/provider.go
@@ -4,8 +4,6 @@ type ProviderConfig struct {
 	Issuer                            string   `json:"issuer"`
 	AuthEndpoint                      string   `json:"authorization_endpoint"`
 	TokenEndpoint                     string   `json:"token_endpoint"`
-	UserInfoEndpoint                  string   `json:"userinfo_endpoint"`
-	RevocationEndpoint                string   `json:"revocation_endpoint"`
 	JWKSURI                           string   `json:"jwks_uri"`
 	ResponseTypesSupported            []string `json:"response_types_supported"`
 	SubjectTypesSupported             []string `json:"subject_types_supported"`

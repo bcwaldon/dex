9704db46536a44636b3d9e7dfa003cb02c58e0a8
Brian Waldon <bcwaldon@gmail.com>
2014-11-12 16:05:06 -0800

    server: test Server.Login

--- server/server_test.go
+++ server/server_test.go
@@ -1,13 +1,15 @@
 package server
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 
+	"github.com/coreos-inc/auth/oauth2"
 	"github.com/coreos-inc/auth/oidc"
 )
 
-func TestProviderConfig(t *testing.T) {
+func TestServerProviderConfig(t *testing.T) {
 	srv := &Server{IssuerURL: "http://server.example.com"}
 
 	want := oidc.ProviderConfig{
@@ -25,5 +27,89 @@ func TestProviderConfig(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("want=%#v, got=%#v", want, got)
+	}
+}
+
+func TestServerLogin(t *testing.T) {
+	ciRepo := NewClientIdentityRepo([]oauth2.ClientIdentity{
+		oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"},
+	})
+
+	signer := &StaticSigner{sig: []byte("beer"), err: nil}
+
+	sm := NewSessionManager("http://server.example.com", signer)
+	sm.generateCode = staticGenerateCodeFunc("fakecode")
+
+	srv := &Server{
+		IssuerURL:          "http://server.example.com",
+		Signer:             signer,
+		SessionManager:     sm,
+		ClientIdentityRepo: ciRepo,
+	}
+
+	ident := oidc.Identity{ID: "YYY", Name: "elroy", Email: "elroy@example.com"}
+	code, err := srv.Login(ident, "XXX")
+	if err != nil {
+		t.Fatalf("Unexpected err from Server.Login: %v", err)
+	}
+
+	if code != "fakecode" {
+		t.Fatalf("Expected code=fakecode, got=%s", code)
+	}
+}
+
+func TestServerLoginUnrecognizedClient(t *testing.T) {
+	ciRepo := NewClientIdentityRepo([]oauth2.ClientIdentity{
+		oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"},
+	})
+
+	signer := &StaticSigner{sig: []byte("beer"), err: nil}
+
+	sm := NewSessionManager("http://server.example.com", signer)
+	sm.generateCode = staticGenerateCodeFunc("fakecode")
+
+	srv := &Server{
+		IssuerURL:          "http://server.example.com",
+		Signer:             signer,
+		SessionManager:     sm,
+		ClientIdentityRepo: ciRepo,
+	}
+
+	ident := oidc.Identity{ID: "YYY", Name: "elroy", Email: "elroy@example.com"}
+	code, err := srv.Login(ident, "123")
+	if err == nil {
+		t.Fatalf("Expected non-nil error")
+	}
+
+	if code != "" {
+		t.Fatalf("Expected empty code, got=%s", code)
+	}
+}
+
+func TestServerLoginNewSessionFails(t *testing.T) {
+	ciRepo := NewClientIdentityRepo([]oauth2.ClientIdentity{
+		oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"},
+	})
+
+	signer := &StaticSigner{sig: nil, err: errors.New("fail")}
+
+	sm := NewSessionManager("http://server.example.com", signer)
+	sm.generateCode = staticGenerateCodeFunc("fakecode")
+
+	srv := &Server{
+		IssuerURL:          "http://server.example.com",
+		Signer:             signer,
+		SessionManager:     sm,
+		ClientIdentityRepo: ciRepo,
+	}
+
+	ident := oidc.Identity{ID: "YYY", Name: "elroy", Email: "elroy@example.com"}
+	code, err := srv.Login(ident, "XXX")
+	if err == nil {
+		t.Fatalf("Expected non-nil error")
+	}
+
+	if code != "" {
+		t.Fatalf("Expected empty code, got=%s", code)
 	}
 }

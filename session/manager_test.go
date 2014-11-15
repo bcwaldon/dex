4165a4a43beb48412c0d52aca24b7cbce86c241f
Brian Waldon <bcwaldon@gmail.com>
2014-11-14 18:14:03 -0800

    session: add missing test coverage

--- session/manager_test.go
+++ session/manager_test.go
@@ -59,3 +59,62 @@ func TestSessionManagerExchangeKey(t *testing.T) {
 		t.Fatalf("Received nil response from attempt with spent Session key")
 	}
 }
+
+func TestSessionManagerGetSessionInStateNoExist(t *testing.T) {
+	sm := NewSessionManager()
+	ses, err := sm.getSessionInState("123", sessionStateNew)
+	if err == nil {
+		t.Errorf("Expected non-nil error")
+	}
+	if ses != nil {
+		t.Errorf("Expected nil Session")
+	}
+}
+
+func TestSessionManagerGetSessionInStateWrongState(t *testing.T) {
+	sm := NewSessionManager()
+	ci := oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"}
+	sessionID := sm.NewSession(ci, "bogus")
+	ses, err := sm.getSessionInState(sessionID, sessionStateDead)
+	if err == nil {
+		t.Errorf("Expected non-nil error")
+	}
+	if ses != nil {
+		t.Errorf("Expected nil Session")
+	}
+}
+
+func TestSessionManagerKill(t *testing.T) {
+	sm := NewSessionManager()
+	ci := oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"}
+	ident := oidc.Identity{ID: "YYY", Name: "elroy", Email: "elroy@example.com"}
+
+	sessionID := sm.NewSession(ci, "bogus")
+
+	if _, err := sm.Identify(sessionID, ident); err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	ses, err := sm.Kill(sessionID)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if ses == nil {
+		t.Fatalf("Expected non-nil Session")
+	}
+
+	if ses.ClientState != "bogus" {
+		t.Errorf("Unexpected Session: %#v", ses)
+	}
+}
+
+func TestSessionManagerKillUnidentified(t *testing.T) {
+	sm := NewSessionManager()
+	ci := oauth2.ClientIdentity{ID: "XXX", Secret: "secrete"}
+
+	sessionID := sm.NewSession(ci, "bogus")
+	_, err := sm.Kill(sessionID)
+	if err == nil {
+		t.Fatalf("Expected non-nil error")
+	}
+}
---
+++ session/repo_test.go
@@ -0,0 +1,107 @@
+package session
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jonboulle/clockwork"
+)
+
+func TestMemSessionKeyRepoPopNoExist(t *testing.T) {
+	r := &memSessionKeyRepo{
+		store: make(map[string]expiringSessionKey),
+		clock: clockwork.NewFakeClock(),
+	}
+
+	_, err := r.Pop("123")
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+}
+
+func TestMemSessionKeyRepoPushPop(t *testing.T) {
+	r := &memSessionKeyRepo{
+		store: make(map[string]expiringSessionKey),
+		clock: clockwork.NewFakeClock(),
+	}
+
+	key := "123"
+	sessionID := "456"
+
+	r.Push(sessionKey{key: key, sessionID: sessionID}, time.Second)
+
+	got, err := r.Pop(key)
+	if err != nil {
+		t.Fatalf("Expected nil error: %v", err)
+	}
+
+	if got != sessionID {
+		t.Fatalf("Incorrect sessionID: want=%s got=%s", sessionID, got)
+	}
+}
+
+func TestMemSessionKeyRepoExpired(t *testing.T) {
+	fc := clockwork.NewFakeClock()
+	r := &memSessionKeyRepo{
+		store: make(map[string]expiringSessionKey),
+		clock: fc,
+	}
+
+	key := "123"
+	sessionID := "456"
+
+	r.Push(sessionKey{key: key, sessionID: sessionID}, time.Second)
+
+	fc.Advance(2 * time.Second)
+
+	_, err := r.Pop(key)
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+}
+
+func TestMemSessionRepoGetNoExist(t *testing.T) {
+	r := &memSessionRepo{
+		store: make(map[string]Session),
+	}
+
+	ses := r.Get("123")
+	if ses != nil {
+		t.Fatalf("Expected nil, got %#v", ses)
+	}
+}
+
+func TestMemSessionRepoSetGet(t *testing.T) {
+	r := &memSessionRepo{
+		store: make(map[string]Session),
+	}
+
+	r.Set(Session{ID: "123", ClientState: "blargh"})
+
+	ses := r.Get("123")
+	if ses == nil {
+		t.Fatalf("Expected non-nil Session")
+	}
+
+	if ses.ClientState != "blargh" {
+		t.Fatalf("Session unrecognized")
+	}
+}
+
+func TestMemSessionRepoSetTwice(t *testing.T) {
+	r := &memSessionRepo{
+		store: make(map[string]Session),
+	}
+
+	r.Set(Session{ID: "123", ClientState: "blargh"})
+	r.Set(Session{ID: "123", ClientState: "boom"})
+
+	ses := r.Get("123")
+	if ses == nil {
+		t.Fatalf("Expected non-nil Session")
+	}
+
+	if ses.ClientState != "boom" {
+		t.Fatalf("Session unrecognized")
+	}
+}

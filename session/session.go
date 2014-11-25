67b18c1ec02c58afdd5bb01e9606f2cb4206dfc1
Brian Waldon <bcwaldon@gmail.com>
2014-11-25 10:07:42 -0800

    session: set exp claim based on Identity

--- session/session.go
+++ session/session.go
@@ -41,14 +41,18 @@ type Session struct {
 }
 
 func (s *Session) Claims(issuerURL string) jose.Claims {
+	exp := s.Identity.ExpiresAt
+	if exp.IsZero() {
+		exp = s.ExpiresAt
+	}
+
 	return jose.Claims{
 		// required
 		"iss": issuerURL,
 		"sub": s.Identity.ID,
 		"aud": s.ClientID,
-		// explicitly cast to float64 for consistent JSON (de)serialization
 		"iat": float64(s.CreatedAt.Unix()),
-		"exp": float64(s.ExpiresAt.Unix()),
+		"exp": float64(exp.Unix()),
 
 		// conventional
 		"name":  s.Identity.Name,
--- session/session_test.go
+++ session/session_test.go
@@ -9,34 +9,66 @@ import (
 	"github.com/coreos-inc/auth/oidc"
 )
 
-func TestSessionIDToken(t *testing.T) {
+func TestSessionClaims(t *testing.T) {
 	issuerURL := "http://server.example.com"
 	now := time.Now().UTC()
-	ses := &Session{
-		CreatedAt: now,
-		ExpiresAt: now.Add(time.Hour),
-		ClientID:  "XXX",
-		Identity: oidc.Identity{
-			ID:    "YYY",
-			Name:  "elroy",
-			Email: "elroy@example.com",
+
+	tests := []struct {
+		ses  Session
+		want jose.Claims
+	}{
+		{
+			ses: Session{
+				CreatedAt: now,
+				ExpiresAt: now.Add(time.Hour),
+				ClientID:  "XXX",
+				Identity: oidc.Identity{
+					ID:    "YYY",
+					Name:  "elroy",
+					Email: "elroy@example.com",
+				},
+			},
+			want: jose.Claims{
+				"iss":   issuerURL,
+				"sub":   "YYY",
+				"aud":   "XXX",
+				"iat":   float64(now.Unix()),
+				"exp":   float64(now.Add(time.Hour).Unix()),
+				"name":  "elroy",
+				"email": "elroy@example.com",
+			},
+		},
+
+		// Identity has custom ExpiresAt
+		{
+			ses: Session{
+				CreatedAt: now,
+				ExpiresAt: now.Add(time.Hour),
+				ClientID:  "XXX",
+				Identity: oidc.Identity{
+					ID:        "YYY",
+					Name:      "elroy",
+					Email:     "elroy@example.com",
+					ExpiresAt: now.Add(time.Minute),
+				},
+			},
+			want: jose.Claims{
+				"iss":   issuerURL,
+				"sub":   "YYY",
+				"aud":   "XXX",
+				"iat":   float64(now.Unix()),
+				"exp":   float64(now.Add(time.Minute).Unix()),
+				"name":  "elroy",
+				"email": "elroy@example.com",
+			},
 		},
 	}
-	want := jose.Claims{
-		// required
-		"iss": issuerURL,
-		"sub": "YYY",
-		"aud": "XXX",
-		"iat": float64(now.Unix()),
-		"exp": float64(now.Add(time.Hour).Unix()),
 
-		// conventional
-		"name":  "elroy",
-		"email": "elroy@example.com",
+	for i, tt := range tests {
+		got := tt.ses.Claims(issuerURL)
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Fatalf("case %d: want=%#v got=%#v", i, tt.want, got)
+		}
 	}
 
-	got := ses.Claims(issuerURL)
-	if !reflect.DeepEqual(want, got) {
-		t.Fatalf("Incorrect claims: want=%#v got=%#v", want, got)
-	}
 }

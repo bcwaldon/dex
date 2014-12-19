83998465ec06e6e136eb2c831f7b94a9ae84e61a
Ed Rooth <ed.rooth@coreos.com>
2014-12-19 15:00:26 +0900

    server: add writeResponseWithBody() function to serialize arbitrary objects

---
+++ server/serialization.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/coreos-inc/auth/pkg/log"
+)
+
+// writeResponseWithBody attempts to marshal an arbitrary thing to JSON then write
+// it to the http.ResponseWriter
+func writeResponseWithBody(w http.ResponseWriter, code int, resp interface{}) {
+	enc, err := json.Marshal(resp)
+	if err != nil {
+		log.Errorf("Failed JSON-encoding HTTP response: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(enc)
+}

6e6d3d236275059c47e19252f9e505c8c96ec055
Ed Rooth <ed.rooth@coreos.com>
2015-01-11 00:09:44 +0900

    integration: test for client creation API using generated bindings

---
+++ integration/client_api_test.go
@@ -0,0 +1,98 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/coreos-inc/auth/oidc"
+	"github.com/coreos-inc/auth/schema"
+)
+
+type tokenHandlerTransport struct {
+	Handler http.Handler
+	Token   string
+}
+
+func (t *tokenHandlerTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", t.Token))
+	w := httptest.NewRecorder()
+	t.Handler.ServeHTTP(w, r)
+	resp := http.Response{
+		StatusCode: w.Code,
+		Header:     w.Header(),
+		Body:       ioutil.NopCloser(w.Body),
+	}
+	return &resp, nil
+}
+
+func TestClientCreate(t *testing.T) {
+	ci := oidc.ClientIdentity{
+		Credentials: oidc.ClientCredentials{
+			ID:     "72de74a9",
+			Secret: "XXX",
+		},
+	}
+	cis := []oidc.ClientIdentity{ci}
+
+	srv, err := mockServer(cis)
+	if err != nil {
+		t.Fatalf("Unexpected error setting up server: %v", err)
+	}
+
+	oidcClient, err := mockClient(srv, ci)
+	if err != nil {
+		t.Fatalf("Unexpected error setting up OIDC client: %v", err)
+	}
+
+	tok, err := oidcClient.ClientCredsToken([]string{"openid"})
+	if err != nil {
+		t.Fatalf("Failed getting client token: %v", err)
+	}
+
+	callbackURL := "http://example.com/oidc/callback"
+	trans := &tokenHandlerTransport{
+		Handler: srv.HTTPHandler(),
+		Token:   tok.Encode(),
+	}
+	hc := &http.Client{
+		Transport: trans,
+	}
+	iss := srv.IssuerURL.String()
+	svc, err := schema.NewWithBasePath(hc, iss)
+	if err != nil {
+		t.Fatalf("Failed creating API service client: %v", err)
+	}
+
+	newClientInput := &schema.Client{
+		RedirectURIs: []string{callbackURL},
+	}
+
+	call := svc.Clients.Create(newClientInput)
+	newClient, err := call.Do()
+	if err != nil {
+		t.Errorf("Call to create client API failed: %v", err)
+	}
+
+	if newClient.Id == "" {
+		t.Error("Expected non-empty Client ID")
+	}
+
+	if newClient.Secret == "" {
+		t.Error("Expected non-empty Client Secret")
+	}
+
+	meta, err := srv.ClientIdentityRepo.Metadata(newClient.Id)
+	if err != nil {
+		t.Errorf("Error looking up client metadata: %v", err)
+	} else if meta == nil {
+		t.Error("Expected new client to exist in repo")
+	}
+
+	gotURL := meta.RedirectURL.String()
+	if gotURL != callbackURL {
+		t.Errorf("Callback URL mismatch, want=%s, got=%s", callbackURL, gotURL)
+	}
+}

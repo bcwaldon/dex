505e1add09e14603c4d4a5fc4d2172dffc9897e2
Bobby Rullo <bobby.rullo@coreos.com>
2015-07-17 11:10:37 -0700

    server: expose version to expvar

--- cmd/authd-worker/main.go
+++ cmd/authd-worker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"flag"
 	"fmt"
 	"net/http"
@@ -13,6 +14,13 @@ import (
 	"github.com/coreos-inc/auth/pkg/log"
 	"github.com/coreos-inc/auth/server"
 )
+
+var version = "DEV"
+
+func init() {
+	versionVar := expvar.NewString("authd.version")
+	versionVar.Set(version)
+}
 
 func main() {
 	fs := flag.NewFlagSet("authd-worker", flag.ExitOnError)
